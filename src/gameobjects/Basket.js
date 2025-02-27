2555a7c945a176cecdd7bff73e07bc60f147ab05
SonderXiaoming <1316864423@qq.com>
2025-02-27 10:54:35 -0600

    center the text of baskets

--- src/gameobjects/Basket.js
+++ src/gameobjects/Basket.js
@@ -8,24 +8,29 @@ export class Basket extends GameObjects.Container {
         this.type = type;
 
         // create bakset image
-        this.basketImage = new GameObjects.Image(scene, 30, 30, "basket");
+        this.basketImage = new GameObjects.Image(scene, 0, 0, "basket");
+        this.basketImage.setOrigin(0.5, 0.5);
         this.basketImage.displayWidth = 100;
         this.basketImage.displayHeight = 100;
 
         // create text label
-        this.textLabel = new GameObjects.Text(scene, 30, -15, type, {
+        this.textLabel = new GameObjects.Text(scene, 0, 0, type, {
             fontSize: "14px",
             fill: "#ffffff",
             backgroundColor: "rgba(0, 0, 0, 0.5)",
             padding: { x: 4, y: 2 },
-            align: "center"
+            align: "center",
+            wordWrap: { width: 100, useAdvancedWrap: true },
         });
-        this.textLabel.setOrigin(0.5, 1);
+        this.textLabel.setOrigin(0.5, 0.5);
 
 
         this.add([this.basketImage, this.textLabel]);
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
+
+        this.body.setSize(this.basketImage.displayWidth, this.basketImage.displayHeight);
+        this.body.setOffset(-this.basketImage.displayWidth / 2, -this.basketImage.displayHeight / 2);
 
         this.body.setImmovable(true);
     }
